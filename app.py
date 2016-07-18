1d54e83a49902e6dfb625853a78961ff28401ab0
Delger Enkhbayar <delger@Delgers-MacBook-Pro.local>
2016-07-18 19:33:44 +0100

    Static

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello(name=None):
-    return "Hello world!"
-#     return render_template('index.html', name=name)
-#
-# @app.route('/aboutme')
-# def hello(name=None):
-#     return render_template('aboutme.html', name=name)
+    return render_template('index.html', name=name)
+
+@app.route('/aboutme')
+def hello(name=None):
+    return render_template('aboutme.html', name=name)
 
 if __name__ == '__main__':
     app.run(port=33507)
