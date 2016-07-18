f68512d254c3a111a6f5d5f8060993049a482f5e
Delger Enkhbayar <delger@Delgers-MacBook-Pro.local>
2016-07-18 19:48:57 +0100

    Style

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask, request, abort, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
+def home():
+    return "Hello, World!"  # return a string
+
+@app.route('/index')
 def hello(name=None):
     return render_template('index.html', name=name)
 
 @app.route('/aboutme')
-def hello(name=None):
+def about(name=None):
     return render_template('aboutme.html', name=name)
 
 if __name__ == '__main__':
