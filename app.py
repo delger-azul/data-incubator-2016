ae2a301c71b8ae08e42674ea99de2e713debf56f
Delger Enkhbayar <delger@Delgers-MacBook-Pro.local>
2016-07-18 20:48:34 +0100

    Button resize

--- app.py
+++ app.py
@@ -2,15 +2,11 @@ from flask import Flask, request, abort, render_template, redirect
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, World!"  # return a string
-
-@app.route('/index')
 def hello(name=None):
     return render_template('index.html', name=name)
 
 @app.route('/aboutme')
-def about(name=None):
+def hello(name=None):
     return render_template('aboutme.html', name=name)
 
 if __name__ == '__main__':
